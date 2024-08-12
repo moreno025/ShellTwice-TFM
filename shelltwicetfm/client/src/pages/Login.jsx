226551b4906153daf3c2df61f783597f0aeb2979
moreno025 <morenopedrejon@gmail.com>
2024-08-12 20:26:23 +0200

    update login/signup jwt

--- shelltwicetfm/client/src/pages/Login.jsx
+++ shelltwicetfm/client/src/pages/Login.jsx
@@ -1,24 +1,35 @@
+import { useNavigate } from 'react-router-dom';
 import styles from '../styles/SignUp.module.css';
 import logo from '../assets/logoEditado.png';
 import LoginForm from '../components/Forms/LoginForm';
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const handleSignUpRedirect = () => {
+        navigate('/signup');
+    };
+
     return (
-        <div className = {`container-fluid px-4 text-center ${styles.container}`}>
+        <div className={`container-fluid px-4 text-center ${styles.container}`}>
             <div className={`row gx-5`}>
+                <LoginForm />
                 <div className={`col`}>
-                    < LoginForm />
-                    <div className= {`p-3`}>
-                        <div className= {`card ${styles.card_left}`}>
+                    <div className={`p-3`}>
+                        <div className={`card ${styles.card_right}`}>
                             <img src={logo} className={`card-img-top ${styles.logo}`} alt="Logo ShellTwice" />
                             <div className={`card-body`}>
-                                <p className={`card-text`}>¿Ya tienes cuenta? Haz <strong><a href=''>Login</a></strong></p>
-                                <a href="#" className= {`btn btn-primary mt-3 ${styles.boton_signup}`}>Login</a>
+                                <p className={`card-text`}>
+                                    ¿Todavía no tienes cuenta? <strong>Regístrate</strong>
+                                </p>
+                                <button onClick={handleSignUpRedirect} className={`btn btn-primary mt-3 ${styles.boton_signup}`}>
+                                    Regístrate
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
-        </div>
+            </div>
         </div>
     );
 };
