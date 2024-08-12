9e2a49a7efc48d09561eba40e61821e7859dbfea
moreno025 <morenopedrejon@gmail.com>
2024-08-12 20:27:03 +0200

    update home con header funcional

---
+++ shelltwicetfm/client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/layouts/Header';
+//import style from '../styles/Home.module.css';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+        </>
+    );
+};
+
+export default Home;
