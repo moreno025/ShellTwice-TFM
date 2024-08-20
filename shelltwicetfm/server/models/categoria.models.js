21152c6cc8383047c6d9268a7d23d7759349c2d7
moreno025 <morenopedrejon@gmail.com>
2024-08-20 12:20:00 +0200

    update models fotos y ubicacion String

--- shelltwicetfm/server/models/categoria.models.js
+++ shelltwicetfm/server/models/categoria.models.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const categoriaSchema = new Schema({
     titulo: { type: String, required: true, unique: true },
+    imagen: { type: String, required: false }
     //numero_anuncios: { type: Number, default: 0 } count sobre el número de anuncios
 }); 
 
